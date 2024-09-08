bb551a2138e3278f107ec30d9d70277fcbd11eb5
Seth Mwebi <sethmwebi27@gmail.com>
2024-09-08 17:16:43 +0300

    adds code to p3-oo-one-to-many lab

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,38 @@
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        if pet_type not in Pet.PET_TYPES:
+            raise Exception(f"{pet_type} is not a valid pet type.")
+        self.name = name
+        self.pet_type = pet_type
+
+        if owner is not None:
+            if not isinstance(owner, Owner):
+                raise Exception("Owner must be an instance of the Owner class")
+            self.owner = owner
+            owner.add_pet(self)
+        else:
+            self.owner = None
+
+        Pet.all.append(self)
+
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self._pets = []
+
+    def pets(self):
+        return self._pets
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("The pet must be an instance of a Pet Class")
+        pet.owner = self
+        self._pets.append(pet)
+
+    def get_sorted_pets(self):
+        return sorted(self._pets, key=lambda pet: pet.name)
+
